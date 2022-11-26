4b805361f8143c7fa9fdd1672c1fc9686bd6759c
Ivan Malyshev <thefuny@mail.ru>
2022-11-26 20:14:55 +0300

    update

--- ARTerrainGeneration/ARTGApp/MainWindow.xaml.cs
+++ ARTerrainGeneration/ARTGApp/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using HeightMapLib;
 
 namespace ARTGApp
 {
@@ -23,6 +24,8 @@ namespace ARTGApp
         public MainWindow()
         {
             InitializeComponent();
+
+            DataContext = new MainViewModel();
         }
     }
 }
